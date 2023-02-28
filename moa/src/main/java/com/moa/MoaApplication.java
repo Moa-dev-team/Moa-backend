b915133740fb919919095d2abba388ce15182633
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-02-28 22:51:25 +0900

    init : spring boot 를 이용하여 프로젝트 생성하였습니다.

---
+++ moa/src/main/java/com/moa/MoaApplication.java
@@ -0,0 +1,13 @@
+package com.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaApplication.class, args);
+	}
+
+}
---
+++ moa/src/test/java/com/moa/MoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
