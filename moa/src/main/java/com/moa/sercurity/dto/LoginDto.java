04ffe797e84beb7ff893c5eea59adf1752991cc8
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-03-01 13:48:50 +0900

    feat : 편의를 위해서 Dto 들과 JwtType 및 Claim Eum 파일을 생성하였습니다.

---
+++ moa/src/main/java/com/moa/sercurity/dto/LoginDto.java
@@ -0,0 +1,18 @@
+package com.moa.sercurity.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginDto {
+
+  private String email;
+  private String password;
+
+}
