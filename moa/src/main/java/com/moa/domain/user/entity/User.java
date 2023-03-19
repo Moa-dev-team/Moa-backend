0f0292adacc2c0ede5b571a6d02a1dfde9d766f5
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-03-19 10:20:27 +0900

    feat : BaseEntity를 적용하였습니다.

--- moa/src/main/java/com/moa/domain/user/entity/User.java
+++ moa/src/main/java/com/moa/domain/user/entity/User.java
@@ -1,39 +1,40 @@
 package com.moa.domain.user.entity;
 
+import com.moa.domain.utility.BaseEntity;
 import com.moa.sercurity.dto.SignupDto;
 import javax.persistence.*;
 import lombok.Getter;
 
 @Getter
 @Entity
-public class User {
+public class User extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(name = "email")
-  private String email;
+    @Column(name = "email")
+    private String email;
 
-  @Column(name = "name")
-  private String name;
+    @Column(name = "name")
+    private String name;
 
-  @Column(name = "password")
-  private String password;
+    @Column(name = "password")
+    private String password;
 
-  @Column(name = "profile")
-  private String profile;
+    @Column(name = "profile")
+    private String profile;
 
-  @Enumerated(EnumType.STRING)
-  private Role role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-  //==생성 메서드==//
-  public static User registerUser(SignupDto signupDto) {
-    User user = new User();
-    user.email = signupDto.getEmail();
-    user.password = signupDto.getPassword();
-    user.role = Role.USER;
+    //==생성 메서드==//
+    public static User registerUser(SignupDto signupDto) {
+        User user = new User();
+        user.email = signupDto.getEmail();
+        user.password = signupDto.getPassword();
+        user.role = Role.USER;
 
-    return user;
-  }
+        return user;
+    }
 }
