bf603cee538011d019315bf5fbbb19c981de0540
Tae Geun Kim <oryukdo3@pusan.ac.kr>
2023-03-19 10:18:21 +0900

    feat : domain의 생성과 수정시간을 공통으로 관리할 BaseEntity를 생성하였습니다.

--- moa/src/main/java/com/moa/MoaApplication.java
+++ moa/src/main/java/com/moa/MoaApplication.java
@@ -2,12 +2,14 @@ package com.moa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MoaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoaApplication.class, args);
+    }
 
 }
---
+++ moa/src/main/java/com/moa/domain/utility/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.moa.domain.utility;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime registerDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+}
