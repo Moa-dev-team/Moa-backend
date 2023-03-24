de8227457f1625c4cdebdaca69a75cbd7ee6ed15
rlaworkd91 <rlaworkd91@naver.com>
2023-03-24 14:48:59 +0900

    feat : 스터디 중간 테이블 생성

---
+++ moa/src/main/java/com/moa/domain/study/entity/study_has_apply_member.java
@@ -0,0 +1,30 @@
+package com.moa.domain.study.entity;
+
+import com.moa.domain.user.entity.User;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class study_has_apply_member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User userId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "study_id")
+  private Study studyId;
+}
