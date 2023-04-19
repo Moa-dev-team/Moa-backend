56ca9e8228ae144490cb9228729260d2aa9854a1
Hosung <an3735297@pusan.ac.kr>
2023-04-19 18:35:39 +0900

    feat : create redis repository

---
+++ moa2/src/main/java/com/moa2/repository/redis/RedisRepository.java
@@ -0,0 +1,30 @@
+package com.moa2.repository.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setWithTimeout(String key, String value, long timeout) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS);
+    }
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    // for test
+    public void deleteAll() {
+        redisTemplate.delete(redisTemplate.keys("*"));
+    }
+}
