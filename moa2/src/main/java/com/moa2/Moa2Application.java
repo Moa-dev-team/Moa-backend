ac3c85d8360bfe8c7453e75333b743d03626af33
Hosung <an3735297@pusan.ac.kr>
2023-04-19 18:02:48 +0900

    feat : create moa2 project

---
+++ moa2/src/main/java/com/moa2/Moa2Application.java
@@ -0,0 +1,13 @@
+package com.moa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moa2Application.class, args);
+	}
+
+}
