68bbcfe5214ff93585246c36c30535df834b23fc
Hosung <an3735297@pusan.ac.kr>
2023-04-19 18:36:52 +0900

    feat : create jwt validator

---
+++ moa2/src/main/java/com/moa2/security/jwt/TokenDto.java
@@ -0,0 +1,14 @@
+package com.moa2.security.jwt;
+
+import lombok.Getter;
+
+@Getter
+public class TokenDto {
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
