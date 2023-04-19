bf85ec9daf7ff025c2dcd127689e2be962281be6
Hosung <an3735297@pusan.ac.kr>
2023-04-19 18:41:51 +0900

    feat : create user details

---
+++ moa2/src/main/java/com/moa2/service/userdetails/MemberDetailsService.java
@@ -0,0 +1,25 @@
+package com.moa2.service.userdetails;
+
+import com.moa2.domain.member.Member;
+import com.moa2.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("there is no user with that email")
+        );
+
+        return new MemberDetails(member);
+    }
+}
