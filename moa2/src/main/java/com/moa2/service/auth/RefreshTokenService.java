563341151de03731d4ca81e087fa4994b422c4e7
Hosung <an3735297@pusan.ac.kr>
2023-04-19 18:36:31 +0900

    feat : create access, refresh token service

---
+++ moa2/src/main/java/com/moa2/service/auth/RefreshTokenService.java
@@ -0,0 +1,43 @@
+package com.moa2.service.auth;
+
+import com.moa2.repository.redis.RedisRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class RefreshTokenService {
+    private final RedisRepository redisRepository;
+
+    private final long refreshTokenValidityInSeconds;
+
+    public RefreshTokenService(RedisRepository redisRepository,
+                               @Value("${jwt.refresh-token-validity-in-seconds}")
+                               long refreshTokenValidityInSeconds) {
+        this.redisRepository = redisRepository;
+        this.refreshTokenValidityInSeconds = refreshTokenValidityInSeconds;
+    }
+
+    private String getKey(String token) {
+        return "refresh_token:" + token;
+    }
+
+    public void setRefreshTokenWithAccessToken(String refreshToken, String accessToken) {
+        redisRepository.setWithTimeout(getKey(refreshToken), accessToken,
+                refreshTokenValidityInSeconds);
+    }
+
+    public String getRefreshToken(String refreshToken) {
+        return redisRepository.get(getKey(refreshToken));
+    }
+
+    public void deleteRefreshToken(String refreshToken) {
+        redisRepository.delete(getKey(refreshToken));
+    }
+
+    public boolean isExistRefreshToken(String refreshToken) {
+        return redisRepository.get(getKey(refreshToken)) != null;
+    }
+
+}
