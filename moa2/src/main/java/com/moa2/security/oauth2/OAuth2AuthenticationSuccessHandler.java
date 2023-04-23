b57360d1c087ea74ae2c07bcd91d9a88127c4ef3
Hosung <an3735297@pusan.ac.kr>
2023-04-23 23:03:35 +0900

    feat : OAuthAuthencticationSuccessHandler

---
+++ moa2/src/main/java/com/moa2/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,48 @@
+package com.moa2.security.oauth2;
+
+import com.moa2.dto.auth.TokenDto;
+import com.moa2.security.jwt.JwtTokenProvider;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
+
+
+        if (response.isCommitted()) {
+            log.debug("Response has already been committed. Unable to redirect to ");
+            return;
+        }
+        clearAuthenticationAttributes(request, response);
+
+        TokenDto tokenDto = jwtTokenProvider.createTokens(authentication);
+        response.setHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
+
+
+    }
+
+
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+}
