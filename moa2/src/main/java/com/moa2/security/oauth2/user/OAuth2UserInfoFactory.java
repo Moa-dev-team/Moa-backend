8a8a071a57a4d36c89875d0f67ec1c8ba61ccb19
Hosung <an3735297@pusan.ac.kr>
2023-04-24 01:30:07 +0900

    feat : add naver ouath2

--- moa2/src/main/java/com/moa2/security/oauth2/user/OAuth2UserInfoFactory.java
+++ moa2/src/main/java/com/moa2/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -11,6 +11,8 @@ public class OAuth2UserInfoFactory {
             return new GithubOAuth2UserInfo(attributes);
         }else if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
             return new GoogleOAuth2UserInfo(attributes);
+        }else if (registrationId.equalsIgnoreCase(AuthProvider.naver.toString())) {
+            return new NaverOAuth2UserInfo(attributes);
         }else {
             throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
         }
