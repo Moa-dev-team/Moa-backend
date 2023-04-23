aff4f2d6a345a0ab3b3f2a634bb264036edea0b6
Hosung <an3735297@pusan.ac.kr>
2023-04-24 00:49:15 +0900

    feat : add google oauth2

--- moa2/src/main/java/com/moa2/security/oauth2/user/OAuth2UserInfoFactory.java
+++ moa2/src/main/java/com/moa2/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -9,7 +9,9 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         if(registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
             return new GithubOAuth2UserInfo(attributes);
-        } else {
+        }else if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        }else {
             throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
         }
     }
