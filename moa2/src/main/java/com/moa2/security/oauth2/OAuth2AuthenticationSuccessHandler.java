ac74e207c4b5b29f13995f31a233008237784a83
Hosung <an3735297@pusan.ac.kr>
2023-04-28 23:43:36 +0900

    fix : OAuth2AuthenticationSuccessHandler
    response json data 를 이미 만들어놓은 ResponseTokenDto format 으로 맞췄습니다..

--- moa2/src/main/java/com/moa2/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ moa2/src/main/java/com/moa2/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -2,6 +2,7 @@ package com.moa2.security.oauth2;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moa2.dto.auth.TokenDto;
+import com.moa2.dto.auth.response.ResponseTokenDto;
 import com.moa2.security.jwt.JwtTokenProvider;
 import com.moa2.service.auth.AuthService;
 import jakarta.servlet.ServletException;
@@ -32,7 +33,6 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
-        String targetUrl = determineTargetUrl(request, response, authentication);
 
         if (response.isCommitted()) {
             log.debug("Response has already been committed. Unable to redirect to ");
@@ -54,19 +54,21 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 .build();
 
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-        Map<String, Object> jsonData = new HashMap<>();
 
-        jsonData.put("accessToken", tokenDto.getAccessToken());
-        jsonData.put("memberId", memberId.toString());
-        jsonData.put("expirationTimeInAccessToken", expirationTimeInAccessToken);
+        ResponseTokenDto responseTokenDto = new ResponseTokenDto(
+                tokenDto.getAccessToken(),
+                memberId,
+                expirationTimeInAccessToken
+        );
 
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonString = objectMapper.writeValueAsString(jsonData);
+        String jsonString = objectMapper.writeValueAsString(responseTokenDto);
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(jsonString);
         response.getWriter().flush();
+        response.getWriter().close();
     }
 
     protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
