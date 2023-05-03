c92f92e05e506c4910ede0e38529e36de6cb6c84
Hosung <an3735297@pusan.ac.kr>
2023-05-03 16:00:06 +0900

    fix : category repository
    category repository 에서 이름으로 카테고리를 찾을 때 optional 로 감싸도록 하였습니다.

--- moa2/src/main/java/com/moa2/repository/memberprofile/CategoryRepository.java
+++ moa2/src/main/java/com/moa2/repository/memberprofile/CategoryRepository.java
@@ -4,7 +4,9 @@ import com.moa2.domain.memberprofile.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByName(String name);
+    Optional<Category> findByName(String name);
 }
--- moa2/src/test/java/com/moa2/repository/memberprofile/ProfileCategoryRepositoryTest.java
+++ moa2/src/test/java/com/moa2/repository/memberprofile/ProfileCategoryRepositoryTest.java
@@ -41,19 +41,19 @@ class ProfileCategoryRepositoryTest {
                 new SocialLink("test socialLink1"), new SocialLink("test socialLink2"));
         memberProfile.setSocialLinks(socialLinks);
 
-        Category backend = categoryRepository.findByName("백엔드");
+        Category backend = categoryRepository.findByName("백엔드").get();
         ProfileJob profileCategory = new ProfileJob();
         profileCategory.setCategory(backend);
         memberProfile.setProfileJobs(List.of(profileCategory));
 
-        Category react = categoryRepository.findByName("React");
-        Category html = categoryRepository.findByName("HTML");
+        Category react = categoryRepository.findByName("React").get();
+        Category html = categoryRepository.findByName("HTML").get();
         List<ProfileInterestTech> profileInterestTechs = List.of(
                 new ProfileInterestTech(react), new ProfileInterestTech(html));
         memberProfile.setProfileInterestTechs(profileInterestTechs);
 
-        Category java = categoryRepository.findByName("Java");
-        Category spring = categoryRepository.findByName("Spring");
+        Category java = categoryRepository.findByName("Java").get();
+        Category spring = categoryRepository.findByName("Spring").get();
         List<ProfileTechStack> profileTechStacks = List.of(
                 new ProfileTechStack(java), new ProfileTechStack(spring));
         memberProfile.setProfileTechStacks(profileTechStacks);
