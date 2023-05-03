afe4b9417cf655d58a3be08cd04c728f882f3891
Hosung <an3735297@pusan.ac.kr>
2023-05-03 15:05:08 +0900

    feat : MemberProfile entity
    Url 과 ProfileCategory 를 상속하는 entity와 MemberProfile entity를 매핑하였습니다.

--- moa2/src/main/java/com/moa2/domain/memberprofile/MemberProfile.java
+++ moa2/src/main/java/com/moa2/domain/memberprofile/MemberProfile.java
@@ -1,10 +1,19 @@
 package com.moa2.domain.memberprofile;
 
+import com.moa2.domain.memberprofile.profilecategory.ProfileInterestTech;
+import com.moa2.domain.memberprofile.profilecategory.ProfileJob;
+import com.moa2.domain.memberprofile.profilecategory.ProfileTechStack;
+import com.moa2.domain.memberprofile.url.SocialLink;
+import com.moa2.domain.memberprofile.url.UrlLink;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -12,4 +21,17 @@ public class MemberProfile {
     @Id @GeneratedValue
     private Long id;
 
+    private String statusMessage;
+
+    @OneToMany
+    private List<UrlLink> urlLinks = new ArrayList<>();
+    @OneToMany
+    private List<SocialLink> socialLinks = new ArrayList<>();
+
+    @OneToMany(mappedBy = "memberProfile")
+    private List<ProfileJob> profileJobs = new ArrayList<>();
+    @OneToMany(mappedBy = "memberProfile")
+    private List<ProfileInterestTech> profileInterestTechs = new ArrayList<>();
+    @OneToMany(mappedBy = "memberProfile")
+    private List<ProfileTechStack> profileTechStacks = new ArrayList<>();
 }
