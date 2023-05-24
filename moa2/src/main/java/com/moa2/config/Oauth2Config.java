e34eeeca4481bf2b5d3860f9e19a25c53a2b1753
Hosung <an3735297@pusan.ac.kr>
2023-05-24 14:56:44 +0900

    feat : Oauth2 Config
    oauth2 yml 에 있는 properties 와 해당 데이터를 저장한 inMemoryProviderRepository 를 사용하기 위해서 oauth2 config 에 관련 세팅을 해줬습니다.

---
+++ moa2/src/main/java/com/moa2/config/Oauth2Config.java
@@ -0,0 +1,27 @@
+package com.moa2.config;
+
+import com.moa2.util.oauth2.InMemoryProviderRepository;
+import com.moa2.util.oauth2.Oauth2Adapter;
+import com.moa2.util.oauth2.Oauth2Properties;
+import com.moa2.util.oauth2.Oauth2Provider;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+@EnableConfigurationProperties(Oauth2Properties.class)
+public class Oauth2Config {
+    private final Oauth2Properties properties;
+
+    public Oauth2Config(Oauth2Properties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public InMemoryProviderRepository inMemoryProviderRepository() {
+        Map<String, Oauth2Provider> providers = Oauth2Adapter.getOauthProviders(properties);
+        return new InMemoryProviderRepository(providers);
+    }
+}
