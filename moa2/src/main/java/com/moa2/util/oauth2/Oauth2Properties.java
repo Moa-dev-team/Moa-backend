530412dc968456a35b135679c878ecc7cb26e75e
Hosung <an3735297@pusan.ac.kr>
2023-05-25 10:16:45 +0900

    fix : variable name

--- moa2/src/main/java/com/moa2/util/oauth2/Oauth2Properties.java
+++ moa2/src/main/java/com/moa2/util/oauth2/Oauth2Properties.java
@@ -3,6 +3,7 @@ package com.moa2.util.oauth2;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +11,12 @@ import java.util.Map;
 @Getter
 @ConfigurationProperties(prefix = "oauth2")
 public class Oauth2Properties {
-    private final Map<String, User> users = new HashMap<>();
-    private final Map<String, Provider> providers = new HashMap<>();
+    private final Map<String, User> user = new HashMap<>();
+    private final Map<String, Provider> provider = new HashMap<>();
 
     @Getter @Setter
     public static class User {
+
         private String clientId;
         private String clientSecret;
         private String redirectUri;
