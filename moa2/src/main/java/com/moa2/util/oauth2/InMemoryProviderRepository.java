af852056664098627f27fd926e0c11eed4dd3529
Hosung <an3735297@pusan.ac.kr>
2023-05-25 10:20:44 +0900

    feat : provider repository
    yml 에 있는 provider 들의 정보를 담는 repository 입니다.

---
+++ moa2/src/main/java/com/moa2/util/oauth2/InMemoryProviderRepository.java
@@ -0,0 +1,19 @@
+package com.moa2.util.oauth2;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class InMemoryProviderRepository {
+    private final Map<String, Oauth2Provider> providers;
+
+    public InMemoryProviderRepository(Map<String, Oauth2Provider> providers) {
+        this.providers = new HashMap<>(providers);
+    }
+
+    public Oauth2Provider findByProviderName(String name) {
+        return providers.get(name);
+    }
+}
