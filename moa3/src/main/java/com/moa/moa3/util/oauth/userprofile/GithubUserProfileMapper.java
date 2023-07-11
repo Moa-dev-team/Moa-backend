cc7d40d2dfc273f6d1c22ed554e9299d6f257c5a
Hosung <an3735297@pusan.ac.kr>
2023-07-11 17:43:37 +0900

    fix : 오타 수정

--- moa3/src/main/java/com/moa/moa3/util/oauth/userprofile/GithubUserProfileMapper.java
+++ moa3/src/main/java/com/moa/moa3/util/oauth/userprofile/GithubUserProfileMapper.java
@@ -8,7 +8,7 @@ public class GithubUserProfileMapper implements UserProfileMapper{
     @Override
     public UserProfile map(Map<String, Object> attributes) {
         return UserProfile.builder()
-                .oauthId(String.valueOf(attributes.get("id")))
+                .oAuthId(String.valueOf(attributes.get("id")))
                 .email(String.valueOf(attributes.get("email")))
                 .name(String.valueOf(attributes.get("name")))
                 .imageUrl(String.valueOf(attributes.get("avatar_url")))
--- moa3/src/main/java/com/moa/moa3/validation/oauth/UserProfileValidator.java
+++ moa3/src/main/java/com/moa/moa3/validation/oauth/UserProfileValidator.java
@@ -7,7 +7,7 @@ import com.moa.moa3.exception.oauth.NotFoundOAuthIdException;
 public class UserProfileValidator {
 
     public static void validate(UserProfile userProfile) throws NotFoundEmailException, NotFoundOAuthIdException {
-        if (userProfile.getoAuthId() == null || userProfile.getoAuthId().isEmpty()) {
+        if (userProfile.getOAuthId() == null || userProfile.getOAuthId().isEmpty()) {
             throw new NotFoundOAuthIdException();
         }
         if (userProfile.getEmail() == null || userProfile.getEmail().isEmpty()) {
