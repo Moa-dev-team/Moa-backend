a0edc600ec857608f3b596e6fa49d182a3ce671c
Hosung <an3735297@pusan.ac.kr>
2023-07-13 19:29:51 +0900

    fix : repository / MemberRepositoryQuerydslImpl
    transactional 을 지정하고 JPAQueryFactory 가 각 함수에서 생성될 수 있게 변경하였습니다.

--- moa3/src/main/java/com/moa/moa3/repository/member/MemberRepositoryQuerydslImpl.java
+++ moa3/src/main/java/com/moa/moa3/repository/member/MemberRepositoryQuerydslImpl.java
@@ -5,17 +5,18 @@ import com.moa.moa3.entity.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.moa.moa3.entity.QMember.*;
 
+@Transactional(readOnly = true)
 abstract class MemberRepositoryQuerydslImpl implements MemberRepositoryQuerydsl{
     @PersistenceContext
     EntityManager em;
 
-    JPAQueryFactory query = new JPAQueryFactory(em);
-
     @Override
     public Member findByName(String name) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
         Member result = query
                 .selectFrom(member)
                 .where(member.name.eq(name))
