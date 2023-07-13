b8846d8038f8a166808654c012c509d6dbd5dc9e
Hosung <an3735297@pusan.ac.kr>
2023-07-13 19:24:52 +0900

    fix : entity / member
    member entity 에 기본 생성자가 없어 추가하였습니다.

--- moa3/src/main/java/com/moa/moa3/entity/Member.java
+++ moa3/src/main/java/com/moa/moa3/entity/Member.java
@@ -4,11 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity{
     @Id
     @GeneratedValue
