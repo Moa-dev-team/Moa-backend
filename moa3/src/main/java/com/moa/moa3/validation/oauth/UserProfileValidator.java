f43ee77251d6e7b59d308f84fdf1c3659c4dbd75
Hosung <an3735297@pusan.ac.kr>
2023-07-15 11:36:38 +0900

    fix : exception / oauth
    email 과 oAuthId 에 대해서 따로 예외 클래스를 만들어 처리해주던걸 기본으로 제공해주는 IllegalArgumentException 으로 통합해서 사용하게 하였습니다.

--- moa3/src/main/java/com/moa/moa3/validation/oauth/UserProfileValidator.java
+++ moa3/src/main/java/com/moa/moa3/validation/oauth/UserProfileValidator.java
@@ -1,19 +1,19 @@
 package com.moa.moa3.validation.oauth;
 
 import com.moa.moa3.dto.oauth.UserProfile;
-import com.moa.moa3.exception.oauth.IllegalEmailException;
-import com.moa.moa3.exception.oauth.IllegalOAuthIdException;
 
 public class UserProfileValidator {
 
     private UserProfileValidator() {}
 
-    public static void validate(UserProfile userProfile) throws IllegalEmailException, IllegalOAuthIdException {
-        if (userProfile.getOAuthId() == null || userProfile.getOAuthId().isEmpty()) {
-            throw new IllegalOAuthIdException();
+    public static void validate(UserProfile userProfile) throws IllegalArgumentException {
+        String oAuthId = userProfile.getOAuthId();
+        String email = userProfile.getEmail();
+        if (oAuthId == null || oAuthId.isEmpty() || oAuthId.isBlank()) {
+            throw new IllegalArgumentException("oAuthId 가 적절하지 않습니다.");
         }
-        if (userProfile.getEmail() == null || userProfile.getEmail().isEmpty()) {
-            throw new IllegalEmailException();
+        if (email == null || email.isEmpty() || email.isBlank()) {
+            throw new IllegalArgumentException("email 이 적절하지 않습니다.");
         }
     }
 }
--- moa3/src/test/java/com/moa/moa3/oauth/OAuthValidationTest.java
+++ moa3/src/test/java/com/moa/moa3/oauth/OAuthValidationTest.java
@@ -1,10 +1,7 @@
 package com.moa.moa3.oauth;
 
 import com.moa.moa3.dto.oauth.UserProfile;
-import com.moa.moa3.exception.oauth.IllegalEmailException;
-import com.moa.moa3.exception.oauth.IllegalOAuthIdException;
 import com.moa.moa3.validation.oauth.UserProfileValidator;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,24 +13,34 @@ public class OAuthValidationTest {
     public void UserProfileValidator_test() {
         // email 이 비어있는 경우
         UserProfile userProfile = new UserProfile("github", "hosung", "", "123");
-        assertThrows(IllegalEmailException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             UserProfileValidator.validate(userProfile);
         });
         // email 이 null 인 경우
         UserProfile userProfile2 = new UserProfile("github", "hosung", null, "123");
-        assertThrows(IllegalEmailException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             UserProfileValidator.validate(userProfile2);
+        });
+        // email 이 공백만 포함하고 있는 경우
+        UserProfile userProfile3 = new UserProfile("github", "hosung", "  ", "123");
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserProfileValidator.validate(userProfile3);
         });
 
         // oAuthId 가 비어있는 경우
-        UserProfile userProfile3 = new UserProfile("", "hosung", "aas@asd", "123");
-        assertThrows(IllegalOAuthIdException.class, () -> {
-            UserProfileValidator.validate(userProfile3);
+        UserProfile userProfile4 = new UserProfile("", "hosung", "aas@asd", "123");
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserProfileValidator.validate(userProfile4);
         });
         // oAuthId 가 null 인 경우
-        UserProfile userProfile4 = new UserProfile(null, "hosung", "aas@asd", "123");
-        assertThrows(IllegalOAuthIdException.class, () -> {
-            UserProfileValidator.validate(userProfile4);
+        UserProfile userProfile5 = new UserProfile(null, "hosung", "aas@asd", "123");
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserProfileValidator.validate(userProfile5);
+        });
+        // oAuthId 가 공백만 포함하고 있는 경우
+        UserProfile userProfile6 = new UserProfile("  ", "hosung", "aas@asd", "123");
+        assertThrows(IllegalArgumentException.class, () -> {
+            UserProfileValidator.validate(userProfile6);
         });
     }
 }
