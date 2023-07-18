5427aa20918c3687447d4f9d0e6dd8427a81dd11
Hosung <an3735297@pusan.ac.kr>
2023-07-18 21:40:43 +0900

    feat : config / security config

--- moa3/src/main/java/com/moa/moa3/config/SecurityConfig.java
+++ moa3/src/main/java/com/moa/moa3/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.moa.moa3.config;
 
+import com.moa.moa3.jwt.JwtAccessDeniedHandler;
+import com.moa.moa3.jwt.JwtAuthenticationEntryPoint;
+import com.moa.moa3.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     private static final String[] PUBLIC_MATCHERS = {
             "/",
@@ -41,11 +53,29 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                    .accessDeniedHandler(jwtAccessDeniedHandler)
+        .and()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .authorizeHttpRequests()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers(PUBLIC_MATCHERS).permitAll()
                 .requestMatchers(SWAGGER_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers( "/swagger-ui/**", "/v3/api-docs/**");
     }
 }
