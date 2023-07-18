7e8f2d31b35756065b592520f72c3708bd8c883c
Hosung <an3735297@pusan.ac.kr>
2023-07-18 21:34:53 +0900

    feat : jwt / JwtTokenProvider
    jwt token 을 제공하는 객체를 만들었습니다.

---
+++ moa3/src/main/java/com/moa/moa3/jwt/JwtTokenProvider.java
@@ -0,0 +1,66 @@
+package com.moa.moa3.jwt;
+
+import com.moa.moa3.security.MemberDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.access-token-validity-in-seconds}")
+    private Long accessTokenValidityInSeconds;
+    @Value("${jwt.refresh-token-validity-in-seconds}")
+    private Long refreshTokenValidityInSeconds;
+    private Key key;
+
+    @PostConstruct
+    protected void init() {
+        key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String createAccessToken(Authentication authentication) {
+        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+
+        Date validity = getDateAfter(this.accessTokenValidityInSeconds);
+
+        return createToken(memberDetails, validity);
+    }
+
+    public String createRefreshToken(Authentication authentication) {
+        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+
+        Date validity = getDateAfter(this.refreshTokenValidityInSeconds);
+
+        return createToken(memberDetails, validity);
+    }
+
+    private Date getDateAfter(long seconds) {
+        return new Date(new Date().getTime() + seconds * 1000);
+    }
+
+    private String createToken(MemberDetails memberDetails, Date validity) {
+        return Jwts.builder()
+                .setSubject(memberDetails.getUsername())
+                .claim("memberId", memberDetails.getMemberId())
+                .claim("authorities", memberDetails.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.joining(",")))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+}
