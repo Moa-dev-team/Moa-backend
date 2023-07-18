d7cdcbb5ea3ce7cc63bb8e260c389872fabc805d
Hosung <an3735297@pusan.ac.kr>
2023-07-18 18:57:15 +0900

    feat : yml / jwt setting

--- moa3/src/main/java/com/moa/moa3/config/SecurityConfig.java
+++ moa3/src/main/java/com/moa/moa3/config/SecurityConfig.java
@@ -12,30 +12,39 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private static final String[] PUBLIC_MATCHERS = {
+            "/",
+            "/favicon.ico",
+            "/**/*.json",
+            "/**/*.xml",
+            "/**/*.properties",
+            "/**/*.woff2",
+            "/**/*.woff",
+            "/**/*.ttf",
+            "/**/*.ttc",
+            "/**/*.ico",
+            "/**/*.bmp",
+            "/**/*.png",
+            "/**/*.gif",
+            "/**/*.svg",
+            "/**/*.jpg",
+            "/**/*.jpeg",
+            "/**/*.html",
+            "/**/*.css",
+            "/**/*.js"
+    };
+
+    private static final String[] SWAGGER_MATCHERS = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/",
-                        "/favicon.ico",
-                        "/**/*.json",
-                        "/**/*.xml",
-                        "/**/*.properties",
-                        "/**/*.woff2",
-                        "/**/*.woff",
-                        "/**/*.ttf",
-                        "/**/*.ttc",
-                        "/**/*.ico",
-                        "/**/*.bmp",
-                        "/**/*.png",
-                        "/**/*.gif",
-                        "/**/*.svg",
-                        "/**/*.jpg",
-                        "/**/*.jpeg",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                .requestMatchers(SWAGGER_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         return http.build();
     }
