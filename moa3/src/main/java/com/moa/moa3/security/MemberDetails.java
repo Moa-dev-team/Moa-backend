0b8dc65f1c7ee178214886f1e19bb5ef05ccb452
Hosung <an3735297@pusan.ac.kr>
2023-07-18 21:39:29 +0900

    feat : security / member details

---
+++ moa3/src/main/java/com/moa/moa3/security/MemberDetails.java
@@ -0,0 +1,56 @@
+package com.moa.moa3.security;
+
+import com.moa.moa3.entity.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class MemberDetails implements UserDetails {
+    private final Member member;
+
+    public Long getMemberId() {
+        return member.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return member.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
