647e01ccfc1dbceba5cf8ed17dbc1c8af320b234
Hosung <an3735297@pusan.ac.kr>
2023-07-24 15:29:48 +0900

    fix : config / security config
    security 가 6.1 버전부터 deprecated 된 메서드가 많아서 이를 수정하였습니다.

--- moa3/src/main/java/com/moa/moa3/config/SecurityConfig.java
+++ moa3/src/main/java/com/moa/moa3/config/SecurityConfig.java
@@ -8,18 +8,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
     private final JwtFilter jwtFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
@@ -51,22 +51,26 @@ public class SecurityConfig {
             "/v3/api-docs/**"
     };
 
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                    .accessDeniedHandler(jwtAccessDeniedHandler)
-        .and()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-            .authorizeHttpRequests()
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/user", "/user/**").hasAuthority("ROLE_USER")
-                .requestMatchers(PUBLIC_MATCHERS).permitAll()
-                .requestMatchers(SWAGGER_MATCHERS).permitAll()
-                .anyRequest().authenticated();
+                .exceptionHandling((exceptionHandling) -> exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler));
+
+        http.sessionManagement((sessionManagement) -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/user", "/user/**").hasAuthority("ROLE_USER")
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                        .requestMatchers(SWAGGER_MATCHERS).permitAll()
+                        .anyRequest().authenticated());
+
         return http.build();
     }
     @Bean
@@ -78,4 +82,5 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers( "/swagger-ui/**", "/v3/api-docs/**");
     }
+
 }
