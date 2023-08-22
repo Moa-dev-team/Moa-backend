9b3347d95284988a0eec89e5ae85babd80ad0242
Hosung <an3735297@pusan.ac.kr>
2023-08-22 19:01:38 +0900

    fix : dto / MemberProfile
    Member 가 Profile 을 일대일로 가지고 Profile 이 일대다로 ProfileSkill 을 가집니다. 이러한 상황에서 ProfileSKill 에 있는 Category 를 조회하는 것은 QueryProjection 으로 어렵다고 판단하여 이를 변경하였습니다.
    향후 조회할 때는 Member 를 조회한 다음 이를 MemberProfile 로 변경하는 방식을 취할 것 같습니다. 이는 이미 조회된 객체들을 한번 더 훑는 것이므로 약간의 성능 저하가 예상됩니다.

--- moa3/src/main/java/com/moa/moa3/dto/member/MemberProfile.java
+++ moa3/src/main/java/com/moa/moa3/dto/member/MemberProfile.java
@@ -1,9 +1,13 @@
 package com.moa.moa3.dto.member;
 
+import com.moa.moa3.entity.member.Member;
+import com.moa.moa3.entity.member.profile.Category;
+import com.moa.moa3.entity.member.profile.ProfileSkill;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 모든 사용자에게 보여지는 프로필 정보 DTO 입니다.
@@ -14,12 +18,13 @@ public class MemberProfile {
     private String email;
     private String imageUrl;
     private LocalDateTime updatedAt;
+    private List<Category> skills;
 
-    @QueryProjection
-    public MemberProfile(String name, String email, String imageUrl, LocalDateTime updatedAt) {
-        this.name = name;
-        this.email = email;
-        this.imageUrl = imageUrl;
-        this.updatedAt = updatedAt;
+    public MemberProfile(Member member) {
+        this.name = member.getName();
+        this.email = member.getEmail();
+        this.imageUrl = member.getImageUrl();
+        this.updatedAt = member.getUpdatedAt();
+        this.skills = member.getProfile().getSkills().stream().map(ProfileSkill::getSkill).toList();
     }
 }
