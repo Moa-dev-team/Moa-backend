f03888853ff2bf929fba0f7449c94f9668b30f87
Hosung <an3735297@pusan.ac.kr>
2023-08-22 18:59:45 +0900

    feat : controller / members

--- moa3/src/main/java/com/moa/moa3/controller/global/GlobalMemberController.java
+++ moa3/src/main/java/com/moa/moa3/controller/global/GlobalMemberController.java
@@ -1,29 +1,25 @@
 package com.moa.moa3.controller.global;
 
-import com.moa.moa3.security.MemberDetails;
+import com.moa.moa3.dto.global.MembersRequestCondition;
 import com.moa.moa3.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/global")
 @RequiredArgsConstructor
 public class GlobalMemberController {
     private final MemberService memberService;
-    @GetMapping("/members")
+    @PostMapping("/members")
     public ResponseEntity getMembers(
             @RequestParam(required = false) String cursor,
-            @RequestParam(defaultValue = "20") int limit
-    ) {
+            @RequestParam(defaultValue = "20") int limit,
+            @RequestBody(required = false) MembersRequestCondition condition
+            ) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(memberService.getMemberList(cursor, limit));
+                .body(memberService.getMemberList(cursor, limit, condition));
     }
 }
