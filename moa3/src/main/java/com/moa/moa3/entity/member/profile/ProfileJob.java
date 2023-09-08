92411083274439935a6e542ee30fc402a642f62b
Hosung <an3735297@naver.com>
2023-09-08 15:47:30 +0900

    feat : entitiy / ProfileJob
    ProfileJob entitiy를 만들었습니다.

---
+++ moa3/src/main/java/com/moa/moa3/entity/member/profile/ProfileJob.java
@@ -0,0 +1,22 @@
+package com.moa.moa3.entity.member.profile;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class ProfileJob {
+    @Id
+    @GeneratedValue
+    @Column(name = "profile_job_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Category job;
+
+    public ProfileJob(Category job) {
+        this.job = job;
+    }
+}
