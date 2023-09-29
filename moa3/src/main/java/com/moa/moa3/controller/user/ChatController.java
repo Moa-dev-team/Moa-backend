4e4a982d59296f72d5808e584001583dc686f30a
Hosung <an3735297@naver.com>
2023-09-29 10:06:36 +0900

    feat : chat / controller / get chat rooms

--- moa3/src/main/java/com/moa/moa3/controller/user/ChatController.java
+++ moa3/src/main/java/com/moa/moa3/controller/user/ChatController.java
@@ -2,17 +2,22 @@ package com.moa.moa3.controller.user;
 
 import com.moa.moa3.dto.chat.CreateChatRequest;
 import com.moa.moa3.dto.chat.CreateChatResponse;
+import com.moa.moa3.security.MemberDetails;
 import com.moa.moa3.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+// 현재 인증, 인가 절차를 진행하고 있지 않지만 향후 포함될 수 있습니다.
 @RestController
 @RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatService chatService;
+
 
     @PostMapping("/create")
     public ResponseEntity createChatRoom(@RequestBody CreateChatRequest createChatRequest) {
@@ -21,5 +26,15 @@ public class ChatController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new CreateChatResponse(roomId));
+    }
+
+    @GetMapping("rooms")
+    public ResponseEntity getChatRooms() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MemberDetails principal = (MemberDetails) authentication.getPrincipal();
+        Long memberId = principal.getMemberId();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(chatService.getChatRooms(memberId));
     }
 }
